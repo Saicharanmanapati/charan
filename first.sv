cdf87afaa72aaf3c9edc3261483d47aafb62b5d5
charanmanapati <manapati2003@gmail.com>
2026-02-20 10:51:52 +0530

    modified env

---
+++ first.sv
@@ -0,0 +1,27 @@
+
+class freq_env_config extends uvm_object;
+  `uvm_object_utils(freq_env_config)
+endclass
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+      `uvm_fatal("GET_freq_env_CFG", "Failed to get freq_env_config")
+pp
+pp
+pp
+pp
+pp
+pp
